c38bd7b6962dab4779af33582501b2457990ecfe
Olusola Ajayi <ebenezerajayi@gmail.com>
2022-08-12 16:49:52 +0100

    attempt to fix test

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -1,11 +1,12 @@
 import rateLimit from 'express-rate-limit';
 
 const apiLimiter = rateLimit({
-  windowMs: 1 * 1 * 1000,
+  windowMs: 1000,
   max: 3,
   message: 'Too many requests from this IP',
   standardHeaders: true,
   legacyHeaders: false,
+  statusCode: 429
 });
 
 export default apiLimiter;
--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -3,14 +3,14 @@ import response from '../utils/response';
 const validateInput = (req, res, next) => {
   const { dob } = req.query;
   if (!dob) {
-    return response(res, 406, 'failed', 'Bad request. Query param dob is required');
+    return response(res, 429, 'failed', 'Bad request. Query param dob is required');
   }
 
   const checkVal = /^[0-9]*$/;
 
   const validDob = checkVal.test(dob);
   if (!validDob) {
-    return response(res, 406, 'failed', 'Bad request. only integers are allowed');
+    return response(res, 429, 'failed', 'Bad request. only integers are allowed');
   }
 
   next();
